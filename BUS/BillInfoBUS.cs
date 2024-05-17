16b25f6df93a75b8bc4b59164b615dfae7b770c8
TranThiThanhPhuong <phuongsuga333@gmail.com>
2024-05-17 21:26:34 +0700

    first commit

---
+++ BUS/BillInfoBUS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class BillInfoBUS
+    {
+        private static BillInfoBUS instance;
+
+        public static BillInfoBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BillInfoBUS();
+                return BillInfoBUS.instance;
+            }
+        }
+        public List<BillInfo2> GetListBillInfoByBillID(int id)
+        {
+            try
+            {
+                return BillInfoDAO.Instance.GetListBillInfoByBillID(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        private BillInfoBUS() { }
+        
+        public void InsertBillInfo(int billID, int foodID, int amount)
+        {
+            try
+            {
+                BillInfoDAO.Instance.InsertBillInfo(billID, foodID, amount);
+            }
+            catch { }
+        }
+
+        public void DeleteBillInfoByBillID(int billID)
+        {
+            try
+            {
+                BillInfoDAO.Instance.DeleteBillInfoByBillID(billID);
+            }
+            catch { }
+        }
+    }
+}
---
+++ DTO/BiiInfo2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BillInfo2
+    {
+        public string FoodName { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public BillInfo2(DataRow row)
+        {
+            this.FoodName = row["FoodName"].ToString();
+            this.Amount = (int)row["Amount"];
+            this.Price = (decimal)row["Price"];
+            this.TotalPrice = (decimal)row["TotalPrice"];
+        }
+    }
+}
